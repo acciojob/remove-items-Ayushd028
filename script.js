415c5443b7eefdcc76802d1efeb1ea55bd3f313e
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-03-11 15:39:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let colorSelect = document.getElementById("colorSelect");
+let button = document.querySelector('input[type="button"]');
+
+button.addEventListener("click", function () {
+    let selectedIndex = colorSelect.selectedIndex; 
+    if (selectedIndex !== -1) { 
+      colorSelect.remove(selectedIndex); 
+    }
+  });
